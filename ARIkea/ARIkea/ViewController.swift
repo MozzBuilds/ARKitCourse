af57cf51d9f716009b651cfbe156ef568256d8a2
Colin Morrison <colin.morrison@reckitt.com>
2023-10-30 16:54:12 +0000

    Section 8 complete

--- ARIkea/ARIkea/ViewController.swift
+++ ARIkea/ARIkea/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet var sceneView: ARSCNView!
     @IBOutlet var collectionVIew: UICollectionView!
+    @IBOutlet var planeDetectedLabel: UILabel!
     
     let config = ARWorldTrackingConfiguration()
     let items = ["cup", "vase", "boxing", "table"]
@@ -28,14 +29,24 @@ class ViewController: UIViewController {
         
         collectionVIew.dataSource = self
         collectionVIew.delegate = self
-//        sceneView.delegate = self
+        sceneView.delegate = self
+        sceneView.autoenablesDefaultLighting = true
         registerGestureRecognisers()
     }
     
     func registerGestureRecognisers() {
+        
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        
+        let pinchGesture = UIPinchGestureRecognizer(target: self, action: #selector(pinch))
+        
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(rotate))
+        
+        longPress.minimumPressDuration = 0.1 /// How long a press should be before the long press gesture is recognised
+        
         sceneView.addGestureRecognizer(tapGesture)
-        /// Whenever the sceneview is tapped on, this function gets called
+        sceneView.addGestureRecognizer(pinchGesture)
+        sceneView.addGestureRecognizer(longPress)
     }
     
     @objc func tapped(sender: UITapGestureRecognizer) {
@@ -52,6 +63,57 @@ class ViewController: UIViewController {
         if !hitTest.isEmpty {
             /// Here we have touched a horizontal surface
             addItem(result: hitTest.first!)
+        }
+    }
+    
+    @objc func pinch(sender: UIPinchGestureRecognizer) {
+        
+        let sceneView = sender.view as! ARSCNView
+        let pinchLocation = sender.location(in: sceneView)
+        let hitTest = sceneView.hitTest(pinchLocation)
+        
+        if !hitTest.isEmpty {
+            /// Get the results from the hit test
+            let results = hitTest.first
+            
+            /// Get the node
+            let node = results?.node
+            
+            /// Scale the node size depending on the pinch
+            let pinchAction = SCNAction.scale(by: sender.scale, duration: 0)
+            node?.runAction(pinchAction)
+           
+            sender.scale = 1.0
+            /// If this is not done, it keeps scaling exponentially from the pinch, non linear scale
+            /// A constant rate scale is good and much easier for a user, than an expotential scale. If they do a big pinch, cup shouldn't increase by x10000 in size
+
+            // Watch for the models used. Sometimes it contains child nodes, all of which must be adjusted to allow scaling at the same rate
+        }
+    }
+    
+    @objc func rotate(sender: UILongPressGestureRecognizer) {
+        
+        let sceneView = sender.view as! ARSCNView
+        let holdLocation = sender.location(in: sceneView)
+        let hitTest = sceneView.hitTest(holdLocation)
+
+        if !hitTest.isEmpty {
+            
+            let result = hitTest.first!
+            
+            if sender.state == .began {
+                /// If I'm currently pressing, start rotating and continue forever
+                let action = SCNAction.rotateBy(x: 0,
+                                                y: CGFloat(GLKMathDegreesToRadians(360)),
+                                                z: 0, duration: 1)
+                
+                let forever = SCNAction.repeatForever(action)
+                result.node.runAction(forever)
+            } else if sender.state == .ended {
+                /// Finger has released
+                /// Stop the animation
+                result.node.removeAllActions()
+            }
         }
     }
     
@@ -75,12 +137,26 @@ class ViewController: UIViewController {
             let thirdColumn = transform.columns.3
             node?.position = SCNVector3(thirdColumn.x, thirdColumn.y, thirdColumn.z)
             
+            if selectedItem == "table" {
+                /// Special case where the table item isn't centered in the scene, so we have centered it manually
+                centerPivot(for: node!)
+            }
+            
             /// Need to make sure the nodes also aren't positioned in the scene
             /// Go to the scene, click the node, inspect, and if it has any x/y/z positions, set them all to zero. Else the node will be offset by that position
             /// They were 0 by default for me
             
             sceneView.scene.rootNode.addChildNode(node!)
         }
+    }
+    
+    /// Nodes aren't always centered in the scene, and this can't be changed in the scene itself
+    func centerPivot(for node: SCNNode) {
+        let min = node.boundingBox.min
+        let max = node.boundingBox.max
+        node.pivot = SCNMatrix4MakeTranslation(min.x + (max.x - min.x)/2,
+                                               min.y + (max.y - min.y)/2,
+                                               min.z + (max.z - min.z)/2)
     }
 
 }
@@ -115,5 +191,23 @@ extension ViewController: UICollectionViewDelegate {
         
         let cell = collectionView.cellForItem(at: indexPath)
         cell?.backgroundColor = .systemOrange
+    }
+}
+
+extension ViewController: ARSCNViewDelegate {
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        /// Called when anchor is added to the scene view, tirggered on a background thread
+        /// Need to check if its a plane anchor that was added
+        guard anchor is ARPlaneAnchor else { return }
+        
+        DispatchQueue.main.async {
+            self.planeDetectedLabel.isHidden = false
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.0, execute: {
+                self.planeDetectedLabel.isHidden = true
+            })
+        }
     }
 }
